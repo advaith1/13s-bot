2a2d5e474fa5425b1525f294ef867bd6348bf992
Evan Jones・原文軒 <48502664+yuenmh@users.noreply.github.com>
2023-04-18 21:53:34 -0700

    converted tabs to spaces

--- index.ts
+++ index.ts
@@ -2,12 +2,12 @@ import { Client, GatewayIntentBits, Events, Partials, ComponentType, TextInputSt
 import { token } from './config.json'
 
 const client = new Client({
-	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessageReactions],
-	partials: [Partials.Message, Partials.Reaction]
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessageReactions],
+    partials: [Partials.Message, Partials.Reaction]
 })
 
 client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`)
+    console.log(`Ready! Logged in as ${c.user.tag}`)
 })
 
 const natalie = '184004745416015872'
@@ -15,57 +15,57 @@ const trueEmoji = '1095068960707330098'
 const falseEmoji = '1097655366814281780'
 
 client.on(Events.MessageReactionAdd, async (reaction, user) => {
-	if (user.id === natalie && reaction.emoji.id === trueEmoji) {
-		const message = await reaction.message.fetch()
+    if (user.id === natalie && reaction.emoji.id === trueEmoji) {
+        const message = await reaction.message.fetch()
 
-		if (message.author.id === natalie) {
-			await reaction.users.remove(user.id)
-			await reaction.message.react(falseEmoji)
-		}
-	}
+        if (message.author.id === natalie) {
+            await reaction.users.remove(user.id)
+            await reaction.message.react(falseEmoji)
+        }
+    }
 })
 
 client.on(Events.InteractionCreate, async interaction => {
-	if (interaction.isButton()) {
-		await interaction.showModal({
-			title: 'Submit Anonymous Feedback',
-			customId: 'feedback',
-			components: [
-				{
-					type: ComponentType.ActionRow,
-					components: [
-						{
-							type: ComponentType.TextInput,
-							customId: 'input',
-							style: TextInputStyle.Paragraph,
-							label: 'Feedback',
-							maxLength: 1996,
-							required: true
-						}
-					]
-				}
-			]
-		})
-	}
+    if (interaction.isButton()) {
+        await interaction.showModal({
+            title: 'Submit Anonymous Feedback',
+            customId: 'feedback',
+            components: [
+                {
+                    type: ComponentType.ActionRow,
+                    components: [
+                        {
+                            type: ComponentType.TextInput,
+                            customId: 'input',
+                            style: TextInputStyle.Paragraph,
+                            label: 'Feedback',
+                            maxLength: 1996,
+                            required: true
+                        }
+                    ]
+                }
+            ]
+        })
+    }
 
-	if (interaction.isModalSubmit()) {
-		const input = interaction.components[0].components[0]
-		if (input.type !== ComponentType.TextInput) return
+    if (interaction.isModalSubmit()) {
+        const input = interaction.components[0].components[0]
+        if (input.type !== ComponentType.TextInput) return
 
-		const feedbackChannel = client.channels.cache.get('1097657507381518459')
-		if (feedbackChannel?.type !== ChannelType.GuildText) return
+        const feedbackChannel = client.channels.cache.get('1097657507381518459')
+        if (feedbackChannel?.type !== ChannelType.GuildText) return
 
-		await feedbackChannel.send({
-			content: `>>> ${input.value}`,
-			allowedMentions: { parse: [] },
-			flags: MessageFlags.SuppressEmbeds
-		})
+        await feedbackChannel.send({
+            content: `>>> ${input.value}`,
+            allowedMentions: { parse: [] },
+            flags: MessageFlags.SuppressEmbeds
+        })
 
-		await interaction.reply({
-			content: 'Thanks for your feedback!',
-			ephemeral: true
-		})
-	}
+        await interaction.reply({
+            content: 'Thanks for your feedback!',
+            ephemeral: true
+        })
+    }
 })
 
 client.login(token)
